7b0a2a1e20d651bc3af58977e7e6c4043fcea961
Vyacheslav <toslavik@gmail.com>
2021-10-22 23:12:28 +0300

    check shake added

--- Magic8Ball/Magic8Ball/Screens/Main/MainViewController.swift
+++ Magic8Ball/Magic8Ball/Screens/Main/MainViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    override var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.becomeFirstResponder()
     }
-
-
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            print("Earth quake...")
+        }
+    }
 }
-
